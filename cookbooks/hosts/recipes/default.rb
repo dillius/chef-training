75cbf25ebc06b5a4d50dd4a2943db265688a0368
Michael Henderson <dillius@gmail.com>
2014-04-29 15:48:28 -0400

    Initial tutorial stuff

---
+++ cookbooks/hosts/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: hosts
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "#{ENV['windir']}\\System32\\drivers\\etc\\hosts" do
+  source "hosts.erb"	
+end
